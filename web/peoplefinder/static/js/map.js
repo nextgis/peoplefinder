0c307a5500c068fed1addd0b5c7828b71228b51a
karavanjo <jownkiv@gmail.com>
2015-07-27 21:19:32 +0300

    #5 map module prototype was implemented

---
+++ web/peoplefinder/static/js/map.js
@@ -0,0 +1,29 @@
+(function ($, pf, L) {
+    pf.modules.map = {};
+    $.extend(pf.modules.map, {
+
+        init: function () {
+            this.setDom();
+            this.buildMap();
+            this.addOsmLayer();
+        },
+
+
+        setDom: function () {
+            pf.view.$map = $('#map');
+        },
+
+
+        buildMap: function () {
+            pf.viewmodel.map = L.map('map').setView([-26.017, 139.219], 4);
+        },
+
+
+        addOsmLayer: function () {
+            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+                maxZoom: 18
+            }).addTo(pf.viewmodel.map);
+        }
+    });
+}(jQuery, pf, L));
