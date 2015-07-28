b6901bd7292c37dbb415295aa26e39c46dfd9f52
drnextgis <rykovd@gmail.com>
2015-07-29 03:17:55 +0600

    implement get_imsi_list

--- web/peoplefinder/__init__.py
+++ web/peoplefinder/__init__.py
@@ -1,9 +1,17 @@
 from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+
+from model.models import (
+    DBSession,
+    Base,
+)
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+
     config = Configurator(settings=settings)
     config.include('pyramid_mako')
     config.add_static_view('static', 'static', cache_max_age=3600)
--- web/peoplefinder/views/imsi.py
+++ web/peoplefinder/views/imsi.py
@@ -1,17 +1,32 @@
 from pyramid.view import view_config
+from sqlalchemy import func
 
+import datetime
+
+from model.models import (
+    DBSession,
+    Measure,
+)
 
 @view_config(route_name='get_imsi_list', renderer='json')
 def get_imsi_list(request):
-    result = [
-        {'id': 1, 'imsi': '410041234567800', 'last_lur': 15},
-        {'id': 2, 'imsi': '410041234567810', 'last_lur': 3},
-        {'id': 3, 'imsi': '430041234567802', 'last_lur': 1},
-        {'id': 4, 'imsi': '210041234234800', 'last_lur': 24},
-        {'id': 5, 'imsi': '550041234567810', 'last_lur': 35},
-        {'id': 6, 'imsi': '120041234567802', 'last_lur': 13},
-        {'id': 7, 'imsi': '430041234234800', 'last_lur': 24}
-    ]
+    result = []
+
+    query = DBSession.query(
+        Measure.id,
+        Measure.imsi,
+        func.max(Measure.timestamp).label("last")
+    ).group_by(Measure.imsi).all()
+
+
+    for measure in query:
+        dtime = datetime.datetime.now() - measure.last
+        result.append({
+            'id': measure.id,
+            'imsi': measure.imsi,
+            'last_lur': dtime.total_seconds() // 60
+        })
+
 
     if 'jtSorting' in request.GET:
         sorting_params = request.GET['jtSorting'].split(' ')
