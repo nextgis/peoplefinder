392fdd7706795488e44730a130c4fa798ee73f74
karavanjo <jownkiv@gmail.com>
2015-08-04 20:19:07 +0300

    #10 imsi was added as element id to IMSI table (tr.td.span path) for fast search row

--- web/peoplefinder/static/js/imsiTable.js
+++ web/peoplefinder/static/js/imsiTable.js
@@ -23,13 +23,17 @@
                     type: 'GET',
                     dataType: 'json'
                 },
-                recordsLoaded: function (event, data) {
+                loadingRecords: function (event, data) {
                     console.log(data);
                 },
                 sorting: true,
                 fields: {
                     imsi: {
-                        title: 'IMSI'
+                        title: 'IMSI',
+                        list: true,
+                        display: function (data) {
+                            return '<span id="' + data.record.imsi + '">' +  data.record.imsi + '</span>';
+                        }
                     },
                     last_lur: {
                         title: 'Last LUR (minutes ago)'
