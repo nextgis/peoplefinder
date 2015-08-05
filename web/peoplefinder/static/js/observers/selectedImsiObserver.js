3cecfa886e98c2cb9e6012d89c925eff77310e21
karavanjo <jownkiv@gmail.com>
2015-08-05 06:03:58 +0300

    #15 selectedImsiObserver was implemented

---
+++ web/peoplefinder/static/js/observers/selectedImsiObserver.js
@@ -0,0 +1,20 @@
+(function ($, pf) {
+    pf.modules.selectedImsiObserver = {};
+    $.extend(pf.modules.selectedImsiObserver, {
+
+        init: function () {
+            pf.viewmodel.selectedImsi = null;
+            this.bindEvents();
+        },
+
+        bindEvents: function () {
+            var vm = pf.viewmodel;
+            pf.subscriber.subscribe('imsi/selected/set', function (imsi) {
+                vm.selectedImsi = imsi;
+                pf.view.$targetImsi.val(imsi);
+                pf.subscriber.publish('observer/sms/list/deactivate');
+                pf.subscriber.publish('observer/sms/list/activate');
+            });
+        }
+    });
+}(jQuery, pf));
