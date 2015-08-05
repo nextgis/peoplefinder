ca2d3b887219dc626942628d89f7747e2b2d2aea
karavanjo <jownkiv@gmail.com>
2015-08-05 06:09:29 +0300

    #16 imsiListObserver calls were added to imsiTable

--- web/peoplefinder/static/js/imsiTable.js
+++ web/peoplefinder/static/js/imsiTable.js
@@ -1,4 +1,4 @@
-(function ($, pf, L) {
+(function ($, pf) {
     pf.modules.imsiTable = {};
     $.extend(pf.modules.imsiTable, {
 
@@ -6,6 +6,7 @@
             this.setDom();
             this.buildTable();
             this.overwriteJTableMethods();
+
         },
 
 
@@ -23,30 +24,35 @@
                     type: 'GET',
                     dataType: 'json'
                 },
-                loadingRecords: function (event, data) {
-                    console.log(data);
-                },
                 selectionChanged: function (event, data) {
                     if (pf.viewmodel.trackingStateActive) return;
+                    var $selectedRows = $(this).jtable('selectedRows'),
+                        imsiSelected;
+                    if ($selectedRows.length > 0) {
+                        imsiSelected = $selectedRows.find('span').attr('id').split('-')[1];
+                        pf.subscriber.publish('imsi/selected/set', [imsiSelected]);
+                    }
                 },
                 selecting: true,
                 multiselect: false,
-                sorting: true,
                 fields: {
                     imsi: {
                         title: 'IMSI',
                         list: true,
                         display: function (data) {
-                            return '<span id="imsi-' + data.record.imsi + '">' +  data.record.imsi + '</span>';
+                            return '<span id="imsi-' + data.record.imsi + '">' + data.record.imsi + '</span>';
                         }
                     },
                     last_lur: {
-                        title: 'Last LUR (minutes ago)'
+                        title: 'Last LUR (minutes ago)',
+                        display: function (data) {
+                            return '<span id="imsi-lur-' + data.record.imsi + '">' + data.record.last_lur + '</span>';
+                        }
                     }
                 }
             });
             pf.viewmodel.imsiTable = pf.view.$imsiTable.data('hik-jtable');
-            pf.viewmodel.imsiTable.load();
+            pf.subscriber.publish('observer/imsi/list/activate');
         },
 
         overwriteJTableMethods: function () {
@@ -54,4 +60,4 @@
             };
         }
     });
-}(jQuery, pf, L));
+}(jQuery, pf));
