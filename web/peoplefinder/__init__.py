8b1dc17255ed8b73e0f97188804e0fdbe4294e22
Alexander Lisovenko <alexander.lisovenko@gmail.com>
2015-08-06 22:28:31 +0300

    Use xmlprc client

--- web/peoplefinder/__init__.py
+++ web/peoplefinder/__init__.py
@@ -1,6 +1,8 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
+import xmlrpclib
+
 from model.models import (
     DBSession,
     Base,
@@ -29,7 +31,14 @@ def main(global_config, **settings):
     config.add_route('get_imsi_messages', 'imsi/{imsi}/messages')
     config.add_route('get_imsi_circles', 'imsi/{imsi}/circles')
 
-    get_peoplefinder_number = lambda request: '10001' # TODO: Get from xmlrpc
+    proxy = xmlrpclib.ServerProxy('http://localhost:8123')
+
+    # get_peoplefinder_number = lambda request: '10001' # TODO: Get from xmlrpc
+    try:
+        get_peoplefinder_number = proxy.get_peoplefinder_number()
+    except xmlrpclib.Error as e:
+        print "ERROR", e
+
     config.add_request_method(get_peoplefinder_number, 'peoplefinder_number', reify=True)
 
     config.scan()
