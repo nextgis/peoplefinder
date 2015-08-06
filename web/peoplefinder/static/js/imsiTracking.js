e5e47250bd25d495340a72dadc53f2888897750e
karavanjo <jownkiv@gmail.com>
2015-08-06 23:46:32 +0300

    #19 "Start tracking" button logic was implemented

--- web/peoplefinder/static/js/imsiTracking.js
+++ web/peoplefinder/static/js/imsiTracking.js
@@ -3,12 +3,38 @@
     $.extend(pf.modules.imsiTracking, {
 
         init: function () {
-            pf.viewmodel.trackingActive = false;
             this.setDom();
+            this.bindEvents();
         },
 
         setDom: function () {
             pf.view.$targetImsi = $('#imsi-input');
+            pf.view.$trackingButton = $('#trackingButton');
+        },
+
+        _buttonText: [
+            'Start Tracking',
+            'Stop Tracking'
+        ],
+        _buttonCss: [
+            'start',
+            'stop'
+        ],
+        _buttonState: 0,
+
+        bindEvents: function () {
+            var context = this;
+            pf.view.$trackingButton.click(function () {
+                if (!pf.viewmodel.selectedImsi) return false;
+                pf.subscriber.publish('observer/tracking/toggle');
+                var newButtonState = context._buttonState === 0 ? 1 : 0;
+                pf.view.$trackingButton.val(context._buttonText[newButtonState]);
+                pf.view.$trackingButton
+                    .removeClass(context._buttonCss[context._buttonState])
+                    .addClass(context._buttonCss[newButtonState])
+                    .text(context._buttonText[newButtonState]);
+                context._buttonState = newButtonState;
+            });
         }
     });
 }(jQuery, pf));
