4f7c134ca2097d96aed7e38f9cf4caa05bc8f676
Alexander Lisovenko <alexander.lisovenko@gmail.com>
2015-08-10 15:13:59 +0300

    improve deploy script

--- web/deploy/ubuntu_14_04_install_deps.sh
+++ web/deploy/ubuntu_14_04_install_deps.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+#add user
+USER_NAME=cloud
+echo Add new user '$USER_NAME', please enter password below...
+sudo adduser $USER_NAME
+sudo usermod -a -G sudo,adm cloud
+
 # default values
-HOME=/home/cloud
+HOME=/home/$USER_NAME
 NGINX_CONF_DIR=/etc/nginx
 SUPERVISOR_CONF_DIR=/etc/supervisor/conf.d
 PEOPLEFINDER_CONF_DIR=/etc/peoplefinder
+PEOPLEFINDER_LOG_DIR=/var/log/peoplefinder
 
 sudo apt-get install aptitude
 sudo apt-get install python-dev
@@ -27,20 +34,25 @@ sudo service supervisor restart
 # create env into home directory
 virtualenv $HOME/env
 
+# create directiories
+sudo mkdir $PEOPLEFINDER_CONF_DIR
+sudo mkdir $PEOPLEFINDER_LOG_DIR
+
 # clone repository
 git clone https://github.com/nextgis/peoplefinder.git "$HOME/peoplefinder"
 
+export PYTHONPATH="${PYTHONPATH}:$HOME/peoplefinder"
+
 # install application
-$HOME/env/bin/pip install -e ./peoplefinder/web
+$HOME/env/bin/pip install -e $HOME/peoplefinder/web
 $HOME/env/bin/pip install uwsgi
 sudo cp "$HOME/peoplefinder/web/development.example.ini" "$PEOPLEFINDER_CONF_DIR/config.ini"
 
 # initialize db
-export PYTHONPATH="${PYTHONPATH}:$HOME/peoplefinder"
 $HOME/env/bin/initialize_peoplefinder_db "$PEOPLEFINDER_CONF_DIR/config.ini"
 
 # setup supervisor
-sudo cp "$HOME/peoplefinder/web/deploy/peoplefinder.conf" "$NGINX_CONF_DIR"
+sudo cp "$HOME/peoplefinder/web/deploy/peoplefinder.conf" "$SUPERVISOR_CONF_DIR"
 sudo cp "$HOME/peoplefinder/com_interface/comms_interface.conf" "$SUPERVISOR_CONF_DIR"
 sudo supervisorctl reread
 sudo supervisorctl update
