6030a219afbfca12e468f1bea4000c920f14c7e7
karavanjo <jownkiv@gmail.com>
2015-08-18 20:23:44 +0300

    #41 refactoring: added trackingButtonClickHandler function

--- web/peoplefinder/static/js/imsiTracking.js
+++ web/peoplefinder/static/js/imsiTracking.js
@@ -23,18 +23,10 @@
         _buttonState: 0,
 
         bindEvents: function () {
-            var context = this;
-            pf.view.$trackingButton.click(function () {
-                if (!pf.viewmodel.selectedImsi) return false;
-                pf.subscriber.publish('observer/tracking/toggle');
-                var newButtonState = context._buttonState === 0 ? 1 : 0;
-                pf.view.$trackingButton.val(context._buttonText[newButtonState]);
-                pf.view.$trackingButton
-                    .removeClass(context._buttonCss[context._buttonState])
-                    .addClass(context._buttonCss[newButtonState])
-                    .text(context._buttonText[newButtonState]);
-                context._buttonState = newButtonState;
-            });
+            pf.view.$trackingButton.click(this.trackingButtonClickHandler);
+        },
+
+        trackingButtonClickHandler: function () {
         }
     });
 }(jQuery, pf));
