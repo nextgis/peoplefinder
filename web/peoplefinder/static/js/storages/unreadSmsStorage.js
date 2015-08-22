77c4095449cf326a937589284f6264ce7c3f9bc4
karavanjo <jownkiv@gmail.com>
2015-08-22 20:00:05 +0300

    #50 select of type storage was implemented

--- web/peoplefinder/static/js/storages/unreadSmsStorage.js
+++ web/peoplefinder/static/js/storages/unreadSmsStorage.js
@@ -1,7 +1,22 @@
 (function ($, pf) {
     pf.modules.unreadSmsStorage = {};
     $.extend(pf.modules.unreadSmsStorage, {
+        _key: 'pf.unreadSms',
+        _unreadSms: {},
+        _storage: null,
         init: function () {
+            this._storage = this._localStorageAvailable() ? $.localStorage : $.cookieStorage;
+        },
+
+        _localStorageAvailable: function () {
+            var test = 'pf.test';
+            try {
+                localStorage.setItem(test, test);
+                localStorage.removeItem(test);
+                return true;
+            } catch (e) {
+                return false;
+            }
         }
     });
 }(jQuery, pf));
