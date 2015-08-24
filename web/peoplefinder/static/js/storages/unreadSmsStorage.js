592e6e43eb0aebd1d3b5721c4603b1b9bacbb776
karavanjo <jownkiv@gmail.com>
2015-08-24 11:53:39 +0300

    #50 createUnreadSmsStorage was implemented

--- web/peoplefinder/static/js/storages/unreadSmsStorage.js
+++ web/peoplefinder/static/js/storages/unreadSmsStorage.js
@@ -1,11 +1,53 @@
 (function ($, pf) {
     pf.modules.unreadSmsStorage = {};
     $.extend(pf.modules.unreadSmsStorage, {
-        _key: 'pf.unreadSms',
-        _unreadSms: {},
+        _key: 'pf:unreadSms',
+        _unreadSms: {'_verify': ''},
         _storage: null,
         init: function () {
             this._storage = this._localStorageAvailable() ? $.localStorage : $.cookieStorage;
+            this.createUnreadSmsStorage();
+        },
+
+        createUnreadSmsStorage: function () {
+            var valueFromStorage = this._storage.get(this._key),
+                verifiedValue = this.verifyValueFromStorage(valueFromStorage);
+
+            if (verifiedValue) {
+                return verifiedValue;
+            } else {
+                this.set(null);
+            }
+        },
+
+        verifyValueFromStorage: function (valueFromStorage) {
+            var jsonStorage = null,
+                valueVerified = false;
+
+            try {
+                jsonStorage = toString.call(valueFromStorage) == '[object String]' ?
+                    JSON.parse(valueFromStorage) :
+                    valueFromStorage;
+                if (jsonStorage.hasOwnProperty('_verify')) {
+                    valueVerified = true;
+                }
+            } catch (e) {
+                console.log('_unreadSms parsing is failed');
+            }
+
+            return valueVerified ? jsonStorage : false;
+        },
+
+        set: function (messagesSmsObj) {
+            var readToWriteObj;
+            if (messagesSmsObj) {
+                readToWriteObj = $.extend({'_verify': ''}, messagesSmsObj);
+            } else {
+                readToWriteObj = {'_verify': ''};
+            }
+
+            this._unreadSms = readToWriteObj;
+            this._storage.set(this._key, JSON.stringify(readToWriteObj));
         },
 
         _localStorageAvailable: function () {
