d44bfc148cb9b366dbecc0316d12ccd64340b13d
karavanjo <jownkiv@gmail.com>
2015-08-24 20:35:54 +0300

    #60 localStorage wrapper was implemented

---
+++ web/peoplefinder/static/js/storages/localStorage.js
@@ -0,0 +1,20 @@
+(function ($, pf) {
+    pf.modules.localStorage = {};
+    $.extend(pf.modules.localStorage, {
+        get: function (key) {
+            return localStorage.getItem(key);
+        },
+
+        getJson: function (key) {
+            return JSON.parse(localStorage.getItem(key));
+        },
+
+        set: function (key, value) {
+             if (typeof value === 'object') {
+                localStorage.setItem(key, JSON.stringify(isObj));
+            } else {
+                localStorage.setItem(key, value);
+            }
+        }
+    });
+}(jQuery, pf));
