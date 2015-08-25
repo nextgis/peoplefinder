464cc582f8ba37f9eb39a5cc1f0a482062122d5a
karavanjo <jownkiv@gmail.com>
2015-08-25 18:10:30 +0300

    settings view was added

--- web/peoplefinder/views/__init__.py
+++ web/peoplefinder/views/__init__.py
@@ -4,3 +4,8 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='home.mako')
 def home(request):
     return {}
+
+
+@view_config(route_name='settings', renderer='settings.mako')
+def settings(request):
+    return {}
