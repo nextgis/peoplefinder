47a980785a97ba3b45c4e59bcdf637f59f6481eb
karavanjo <jownkiv@gmail.com>
2015-09-02 05:36:22 +0300

    #37 tilesDownloader module was mplemented

--- web/peoplefinder/static/js/settings/tilesDownloader.js
+++ web/peoplefinder/static/js/settings/tilesDownloader.js
@@ -2,9 +2,47 @@
     pf.modules.tilesDownloader = {};
     $.extend(pf.modules.tilesDownloader, {
 
-
+        $tilesDownloader: null,
 
         init: function () {
+            this.setDom();
+            this.bindEvents();
+        },
+
+        setDom: function () {
+            this.$tilesDownloader = $('#tilesDownloader');
+        },
+
+        bindEvents: function () {
+            this.$tilesDownloader.click($.proxy(function () {
+
+            }, this));
+
+            pf.subscriber.subscribe('/tiles/downloading/status/changed', function (status) {
+                this.updateDownloadingButton(status);
+            }, this);
+        },
+
+        downloadingButtonClickHandler: function () {
+            var status = this.$tilesDownloader.data('status') === 'ready' ? 'downloading' : 'ready';
+            pf.subscriber.subscribe('observer/tiles/downloading/status/deactivate');
+                var status =
+                this.toggleDownloadingButton();
+        },
+
+        updateDownloadingButton: function (status) {
+            if (!status) {
+
+            }
+
+            switch (status){
+                case 'ready':
+                    this.$tilesDownloader.text = 'Download tiles';
+                    break;
+                case 'downloading':
+                    this.$tilesDownloader.text = 'Stop downloading';
+                    break;
+            }
 
         }
     });
