8d98ccc3ece3b13a0faf3da2762c0c052d8f76b7
karavanjo <jownkiv@gmail.com>
2015-09-02 07:15:44 +0300

    #37 added ajax calls and handlers to tilesDownloader module

--- web/peoplefinder/static/js/settings/tilesDownloader.js
+++ web/peoplefinder/static/js/settings/tilesDownloader.js
@@ -3,10 +3,12 @@
     $.extend(pf.modules.tilesDownloader, {
 
         $tilesDownloader: null,
+        _state: null,
 
         init: function () {
             this.setDom();
             this.bindEvents();
+            this.setInitialStatus();
         },
 
         setDom: function () {
@@ -15,7 +17,11 @@
 
         bindEvents: function () {
             this.$tilesDownloader.click($.proxy(function () {
-
+                if (this._state === 'ready') {
+                    this.startDownload();
+                } else if (this._state === 'downloading') {
+                    this.stopDownload();
+                }
             }, this));
 
             pf.subscriber.subscribe('/tiles/downloading/status/changed', function (status) {
@@ -23,27 +29,89 @@
             }, this);
         },
 
+        setInitialStatus: function () {
+            $.ajax({
+                url: pf.settings.root_url + '/tiles/download/status',
+                dataType: 'json',
+                type: 'GET'
+            }).done($.proxy(function (result) {
+                this._state = result.status;
+                this.updateDownloadingButton(this._state);
+                pf.subscriber.publish('observer/tiles/downloading/status/activate');
+
+            }, this));
+        },
+
         downloadingButtonClickHandler: function () {
             var status = this.$tilesDownloader.data('status') === 'ready' ? 'downloading' : 'ready';
             pf.subscriber.subscribe('observer/tiles/downloading/status/deactivate');
-                var status =
+            var status =
                 this.toggleDownloadingButton();
         },
 
         updateDownloadingButton: function (status) {
-            if (!status) {
+            if (!status) { return false; }
 
-            }
-
-            switch (status){
+            switch (status) {
                 case 'ready':
-                    this.$tilesDownloader.text = 'Download tiles';
+                    this.$tilesDownloader.val('Download tiles');
                     break;
                 case 'downloading':
-                    this.$tilesDownloader.text = 'Stop downloading';
+                    this.$tilesDownloader.val('Stop downloading');
                     break;
             }
 
+        },
+
+        startDownload: function () {
+            var map = pf.viewmodel.map,
+                bounds = map.getBounds(),
+                params = {
+                    bounds: [
+                        bounds._southWest.lng,
+                        bounds._southWest.lat,
+                        bounds._northEast.lng,
+                        bounds._northEast.lat].join(','),
+                    zoom: map.getZoom()
+                };
+
+            pf.subscriber.publish('observer/tiles/downloading/status/deactivate');
+            this.$tilesDownloader.val('Starting...');
+            this.$tilesDownloader.prop('disabled', true);
+
+            $.ajax({
+                url: pf.settings.root_url + '/tiles/download/start',
+                dataType: 'json',
+                type: 'POST',
+                data: params
+            }).done($.proxy(function () {
+                setTimeout($.proxy(function () {
+                    this._state = 'downloading';
+                    this.$tilesDownloader.val('Stop downloading');
+                    this.$tilesDownloader.prop('disabled', false);
+                    pf.subscriber.publish('observer/tiles/downloading/status/activate');
+                }, this), 2000);
+            }, this));
+        },
+
+
+        stopDownload: function () {
+            pf.subscriber.publish('observer/tiles/downloading/status/deactivate');
+            this.$tilesDownloader.val('Stopping...');
+            this.$tilesDownloader.prop('disabled', true);
+
+            $.ajax({
+                url: pf.settings.root_url + '/tiles/download/stop',
+                dataType: 'json',
+                type: 'GET'
+            }).done($.proxy(function () {
+                setTimeout($.proxy(function () {
+                    this._state = 'ready';
+                    this.$tilesDownloader.val('Download tiles');
+                    this.$tilesDownloader.prop('disabled', false);
+                    pf.subscriber.publish('observer/tiles/downloading/status/activate');
+                }, this), 2000);
+            }, this));
         }
     });
 }(jQuery, pf, L));
